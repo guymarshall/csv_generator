dc6276a37d882d89f12426dce84bf897597f6079
Guy Marshall <guymarshall9@gmail.com>
2023-01-29 20:21:07 +0000

    Add random_room to random.rs

--- src/random.rs
+++ src/random.rs
@@ -1,3 +1,4 @@
+use std::ops::RangeInclusive;
 use rand::prelude::*;
 use rand::distributions::uniform::Uniform;
 
@@ -29,4 +30,12 @@ fn random_day(include_weekends: bool) -> String {
     };
     let index: usize = rng.sample(range);
     days_of_week[index].to_string()
+}
+
+fn random_room() -> &'static str {
+    let rooms: [&str; 49] = ["Ma1", "Ma2", "Ma3", "Ma4", "Ma5", "Ma6", "Ma7", "Ma8", "Ma9", "DT1", "DT2", "DT3", "DT4", "DT5", "IT1", "IT2", "IT3", "La1", "La2", "La3", "La4", "La5", "History1", "History2", "History3", "Geography1", "Geography2", "Geography3", "Sc1", "Sc2", "Sc3", "Sc4", "Sc5", "Sc6", "Sc7", "Sc8", "Eng1", "Eng2", "Eng3", "Eng4", "Eng5", "Eng6", "Eng7", "Eng8", "Music1", "Music2", "Drama1", "Drama2", "PE"];
+
+    let mut rng: ThreadRng = thread_rng();
+    let range: RangeInclusive<usize> = 0..=rooms.len() - 1;
+    &rooms[rng.gen_range(range)]
 }
