f86d40d7b9f59b539e2e111479abf1b6153e0973
Guy Marshall <guymarshall9@gmail.com>
2023-01-29 18:47:44 +0000

    Remove get_random_name from functions.rs commented code

--- src/functions.rs
+++ src/functions.rs
@@ -1,16 +1,4 @@
 /*
-// <?php
-//
-// function get_random_name(string $filename)
-// {
-//     $contents = file_get_contents($filename);
-//     $first_names = explode("\n", $contents);
-//
-//     $key = array_rand($first_names);
-//     $first_name = $first_names[$key];
-//     return $first_name;
-// }
-//
 // function generate_csv(string $filename, array $field_headings, array $data)
 // {
 //     $file = fopen($filename, 'w');
