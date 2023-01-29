7f4b1fe5229239319cc98b2aa814b0ba1753ce95
Guy Marshall <guymarshall9@gmail.com>
2023-01-29 18:09:39 +0000

    Remove unnecessary path prefix for thread_rng

--- src/random.rs
+++ src/random.rs
@@ -2,7 +2,7 @@ use rand::prelude::*;
 use rand::distributions::uniform::Uniform;
 
 pub fn generate_random_number(min: i32, max: i32, return_string: bool) -> String {
-    let mut rng: ThreadRng = rand::thread_rng();
+    let mut rng: ThreadRng = thread_rng();
     let range: Uniform<i32> = Uniform::from(min..=max);
     let result: i32 = range.sample(&mut rng);
     if return_string {
