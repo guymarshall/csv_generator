8a0f09751e27bdc33357c2a72f4c968dadc0680b
Guy Marshall <guymarshall9@gmail.com>
2023-01-29 19:00:08 +0000

    Remove random_name from functions.rs commented code

--- src/functions.rs
+++ src/functions.rs
@@ -12,21 +12,6 @@
 //     fclose($file);
 // }
 //
-// function random_day(bool $include_weekend = false)
-// {
-//     $days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-//
-//     if ($include_weekend)
-//     {
-//         $days_of_week[] = 'Saturday';
-//         $days_of_week[] = 'Sunday';
-//     }
-//
-//     $max_random_index = $include_weekend ? 4 : 6;
-//
-//     return $days_of_week[mt_rand(0, $max_random_index)];
-// }
-//
 // function random_room()
 // {
 //     $rooms = ['Ma1', 'Ma2', 'Ma3', 'Ma4', 'Ma5', 'Ma6', 'Ma7', 'Ma8', 'Ma9', 'DT1', 'DT2', 'DT3', 'DT4', 'DT5', 'IT1', 'IT2', 'IT3', 'La1', 'La2', 'La3', 'La4', 'La5', 'History1', 'History2', 'History3', 'Geography1', 'Geography2', 'Geography3', 'Sc1', 'Sc2', 'Sc3', 'Sc4', 'Sc5', 'Sc6', 'Sc7', 'Sc8', 'Eng1', 'Eng2', 'Eng3', 'Eng4', 'Eng5', 'Eng6', 'Eng7', 'Eng8', 'Music1', 'Music2', 'Drama1', 'Drama2', 'PE'];
