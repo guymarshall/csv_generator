3565622bbcfa5be740b61e491aa5dfa6a9d21167
Guy Marshall <guymarshall9@gmail.com>
2023-01-29 18:47:17 +0000

    Add get_random_name to random.rs

--- src/random.rs
+++ src/random.rs
@@ -11,3 +11,10 @@ pub fn generate_random_number(min: i32, max: i32, return_string: bool) -> String
         result.to_string()
     }
 }
+
+fn get_random_name(names: &Vec<&str>) -> String {
+    let mut rng: ThreadRng = thread_rng();
+    let range: Uniform<usize> = Uniform::new(0, names.len());
+    let index: usize = rng.sample(range);
+    names[index].to_string()
+}
