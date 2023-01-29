b8e5354be6fe794a04dfee678cab837b147b707c
Guy Marshall <guymarshall9@gmail.com>
2023-01-29 14:23:11 +0000

    Add type annotations to variables in generate_random_number

--- src/random.rs
+++ src/random.rs
@@ -2,9 +2,9 @@ use rand::prelude::*;
 use rand::distributions::uniform::Uniform;
 
 pub fn generate_random_number(min: i32, max: i32, return_string: bool) -> String {
-    let mut rng = rand::thread_rng();
-    let range = Uniform::from(min..=max);
-    let result = range.sample(&mut rng);
+    let mut rng: ThreadRng = rand::thread_rng();
+    let range: Uniform<i32> = Uniform::from(min..=max);
+    let result: i32 = range.sample(&mut rng);
     if return_string {
         result.to_string()
     } else {
