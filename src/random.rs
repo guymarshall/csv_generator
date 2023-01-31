89784cf6f720a2293d0e820e10e82d4550a215ef
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-01-31 12:19:48 +0000

    Rename generate_random_number to random_number

--- src/random.rs
+++ src/random.rs
@@ -2,7 +2,7 @@ use std::ops::{Range, RangeInclusive};
 use rand::prelude::*;
 use rand::distributions::uniform::Uniform;
 
-pub fn generate_random_number(min: i32, max: i32, return_string: bool) -> String {
+pub fn random_number(min: i32, max: i32, return_string: bool) -> String {
     let mut rng: ThreadRng = thread_rng();
     let range: Uniform<i32> = Uniform::from(min..=max);
     let result: i32 = range.sample(&mut rng);
