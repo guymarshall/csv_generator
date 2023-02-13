933d6055c91c3bb64ca80c0c05debb8bf1f08c6a
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-02-13 12:16:51 +0000

    Update prompts to use input() instead of get_user_input()

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,13 @@ mod user_input;
 
 fn main() {
     println!("CSV Generator - Enter counts for the following prompts to generate your .CSV file.");
-    let curriculum_count: i32 = user_input::get_user_input("Curriculum Count:");
-    let period_schedule_count: i32 = user_input::get_user_input("Period Schedule Count:");
-    let room_count: i32 = user_input::get_user_input("Room Count:");
-    let student_count: i32 = user_input::get_user_input("Student Count:");
-    let subject_count: i32 = user_input::get_user_input("Subject Count:");
-    let teacher_count: i32 = user_input::get_user_input("Teacher Count:");
-    let teacher_type_count: i32 = user_input::get_user_input("Teacher Type Count:");
+    let curriculum_count: i32 = user_input::input("Curriculum Count:");
+    let period_schedule_count: i32 = user_input::input("Period Schedule Count:");
+    let room_count: i32 = user_input::input("Room Count:");
+    let student_count: i32 = user_input::input("Student Count:");
+    let subject_count: i32 = user_input::input("Subject Count:");
+    let teacher_count: i32 = user_input::input("Teacher Count:");
+    let teacher_type_count: i32 = user_input::input("Teacher Type Count:");
 
     if curriculum_count < 1
         || period_schedule_count < 1
