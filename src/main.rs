b0ee8aac405f015db790cf3806e3490958f35e07
Guy Marshall <guymarshall9@gmail.com>
2023-05-08 20:51:34 +0100

    UPDATE: Change parameter type from &str to String for generate_room_csv

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ fn main() {
 		period_schedule_data
 	);
 
-	let mut room_data: Vec<Vec<(i32, &str, i32)>> = vec![];
+	let mut room_data: Vec<Vec<(i32, String, i32)>> = vec![];
     for i in 0..room_count {
 		room_data.push(vec![(
 			i + 1,
